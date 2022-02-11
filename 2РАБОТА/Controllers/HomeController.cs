d093346bdb598b93a63022e0ea3dc96569d0365d
yanikitosik <nwcbrig@yandex.ru>
2022-02-11 17:31:01 +0300

    lab1

---
+++ 2РАБОТА/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using _2РАБОТА.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2РАБОТА.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Moning" : "Good Afternoon";
+            return View();
+
+        }
+        [HttpGet]
+        public IActionResult ContactForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FormContact(Contact contact)
+        {
+            using (ContactsContext db = new ContactsContext())
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+            }
+            return View("ContactAdded", contact);
+
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult ContactsList()
+        {
+            using (ContactsContext db = new ContactsContext())
+            {
+                ViewBag.Contacts = db.Contacts.ToList();
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 2РАБОТА/Models/ContactsContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2РАБОТА.Models
+{
+    public class ContactsContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ContactsContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JA8GRE2;Database=rabotadva;Trusted_Connection=True;");
+        }
+    }
+
+}
