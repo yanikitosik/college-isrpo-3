301bb61d12d300fb7445ed942da1252fbf6c325f
yanikitosik <nwcbrig@yandex.ru>
2022-02-11 17:33:13 +0300

    lab 2

---
+++ 4работа/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _4работа
+{
+    class Program
+    {
+        const int ECHO_PORT = 8080;
+        public static int nClients = 0;
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener clientListener = new TcpListener(ECHO_PORT);
+                clientListener.Start();
+                Console.WriteLine("Waiting for connections...");
+                while (nClients < 3)
+                {
+                    TcpClient client = clientListener.AcceptTcpClient();
+                    СlientHandler cHadnler = new СlientHandler();
+                    cHadnler.clientSocket = client;
+                    Thread clientThread = new Thread(new ThreadStart(cHadnler.RunClient));
+                    clientThread.Start();
+                    nClients++;
+                }
+                clientListener.Stop();
+            }
+            catch(Exception exp)
+            {
+                Console.WriteLine("Exception:" + exp);
+            }
+        }
+        public static string GetLocalIPAdress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach(var ip  in host.AddressList)
+            {
+                if(ip.AddressFamily==AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+
+                }
+            }
+            throw new Exception("NO network adapters with an IPv4 address in the system!");
+        }
+    }
+}
---
+++ 4работа/СlientHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4работа
+{
+   public class СlientHandler
+    {
+        public TcpClient clientSocket;
+        public void RunClient()
+        {
+            StreamReader readerStream = new StreamReader(clientSocket.GetStream());
+            NetworkStream writerStream = clientSocket.GetStream();
+            string returnData = readerStream.ReadLine();
+            string name = returnData;
+            Console.WriteLine("Welcome" + name + "to server");
+            while(true)
+            {
+                returnData = readerStream.ReadLine();
+                if(returnData.IndexOf("QUIT")>-1)
+                {
+                    Console.WriteLine("Goodbuy" + name);
+                    break;
+                }
+                Console.WriteLine(name + " : " + returnData);
+                returnData += "\r\n";
+                byte[] dataWrite = Encoding.ASCII.GetBytes(returnData);
+                writerStream.Write(dataWrite, 0, dataWrite.Length);
+            }
+        }
+    }
+}
